9ab7884b7964d70dc670a444e2c9573e4e228353
Dmitri Kolytchev <kolytchev.d@gmail.com>
2016-05-20 18:21:40 -0400

    bunch of shit

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ $(function($){
 	'use strict';
 
 	var arcDivisions = 30;
+	var pivot = 3;
 	//var arcColors = ['red', 'green', 'blue'];
 
+	/** Basic Animator class, takes drawables and gives them a canvas to draw on
+	  *can be made to Update at 60fps by uncommenting "this.timer = setTimeout"
+	  *modified for Cedille use because we don't need constant drawing.
+	  */
 	var Animator = function(canvas){
 		this.drawables = [];
 		this.canvas = canvas;
 		this.context = canvas.getContext('2d');
 		this.skipped = 0;
 		this.onALeTemps = true;
+		this.fps = 30;
 	};
 	Animator.prototype.draw = function(){
 
@@ -30,7 +36,7 @@ $(function($){
 			this.skipped = 0;
 		}else this.skipped++;
 
-		//this.timer = window.setTimeout(this.update.bind(this), 16);
+		this.timer = window.setTimeout(this.update.bind(this), 1000/this.fps);
 	};
 	Animator.prototype.start = function () {
 		if(!this.timer)
@@ -41,15 +47,18 @@ $(function($){
 		this.slices = [];
 		this.innerRadius = 20;
 		this.outerRadius = 80;
+		this.totalPortions = 0;
 	};
-	PiegressChart.prototype.addSlice = function (title, percentage, color) {
+	PiegressChart.prototype.addSlice = function (title, percentage, portion) {
 		this.slices.push({
 			title: 		title||"[Untitled]",
-			percentage: percentage,
-			color: 		color|| "silver"
+			portion: 	portion,
+			percentage: percentage
 		});
+		this.totalPortions += portion;
 	};
 	PiegressChart.prototype.draw = function (c, elem) {
+		console.log(this.totalPortion);
 		var baseRadius = Math.min(window.innerWidth, window.innerHeight);
 		var innerRadius = this.innerRadius/200 * baseRadius;
 		var outerRadius = this.outerRadius/200 * baseRadius;
@@ -59,34 +68,133 @@ $(function($){
 			y:elem.height/2
 		};
 
-		c.lineTo(center.x, center.y);
+		c.moveTo(center.x, center.y);
 
-		var slice = Math.PI * 2 / this.slices.length;
+		var slice = Math.PI * 2 / this.totalPortions;
 		var subslice = slice / arcDivisions;
+		var portionsToDate = 0;
+
+		// draw a background dropshadow for aesthetics
+		c.shadowOffsetY = 6;
+		c.shadowBlur = outerRadius * 0.05;
+		c.shadowColor = "rgba(0, 0, 0, 0.65)";
+		c.moveTo(window.innerWidth, center.y);
+		c.lineTo(window.innerWidth, 0)
+		c.lineTo(0, 0)
+		c.lineTo(0, window.innerHeight)
+		c.lineTo(window.innerWidth, window.innerHeight)
+		c.lineTo(window.innerWidth, center.y);
+		c.arc(center.x, center.y, outerRadius + pivot + 6, 0, Math.PI*2);
+
+		c.moveTo(center.x + innerRadius, center.y);
+		c.arc(center.x, center.y, innerRadius + pivot - 6, 0, Math.PI*2);
+		c.fillStyle = $('body').css('background-color');
+		c.fill();
+
+		c.moveTo(center.x + innerRadius, center.y);
+		c.arc(center.x, center.y, innerRadius + pivot - 6, 0, Math.PI*2);
+		c.shadowBlur = c.shadowOffsetX = c.shadowOffsetY = 0;
+
+		c.beginPath();
+		c.arc(center.x, center.y, outerRadius + pivot + 7.5, 0, Math.PI*2);
+		c.strokeStyle = "white";
+		c.stroke();
+
+		/**
+		* For each slice of data, draw a wedge. Its radial width represents the percentage of commits associated with it,
+		* its inner (more colored) wedge represents the progress done on it (issues completed vs issues total)
+		**/
 		for(var i = 0, ii = this.slices.length; i<ii; i++){
-			c.beginPath();
+			var sliceData = this.slices[i];
+			var color = 360 / this.slices.length * i;
 			var offset = {
-				x: Math.cos(Math.PI*2/this.slices.length*(i+0.5)) * 2,
-				y: Math.sin(Math.PI*2/this.slices.length*(i+0.5)) * 2
+				x: Math.cos((portionsToDate + sliceData.portion*0.5)/this.totalPortions * Math.PI*2) * pivot,
+				y: Math.sin((portionsToDate + sliceData.portion*0.5)/this.totalPortions * Math.PI*2) * pivot,
 			}
-			for(var j = 0, jj = arcDivisions; j<=jj; j++){
+
+			c.beginPath();
+			for(var j = 0, jj = arcDivisions; j<=jj; j++){ // draw the small inner arc
 				c.lineTo(
-					center.x + offset.x + Math.cos(slice * i + subslice * j) * innerRadius,
-					center.y + offset.y + Math.sin(slice * i + subslice * j) * innerRadius
+					center.x + offset.x + Math.cos(subslice * j * sliceData.portion + slice * portionsToDate) * innerRadius,
+					center.y + offset.y + Math.sin(subslice * j * sliceData.portion + slice * portionsToDate) * innerRadius
 				)
 			}
-			for(var j = 0, jj = arcDivisions; j<=jj; j++){
+			for(var j = 0, jj = arcDivisions; j<=jj; j++){ // draw the big outer arc
 				c.lineTo(
-					center.x + offset.x + Math.cos(slice * i + subslice * (arcDivisions-j)) * outerRadius,
-					center.y + offset.y + Math.sin(slice * i + subslice * (arcDivisions-j)) * outerRadius
+					center.x + offset.x + Math.cos(subslice * (arcDivisions-j) * sliceData.portion + slice * portionsToDate) * outerRadius,
+					center.y + offset.y + Math.sin(subslice * (arcDivisions-j) * sliceData.portion + slice * portionsToDate) * outerRadius
 				)
 			}
-			c.fillStyle = 'hsla('+ 360 / this.slices.length * i +', 75%, 50%, 0.5)';
-			c.strokeStyle = 'hsla('+ 360 / this.slices.length * i +', 100%, 25%, 1)';
+			c.fillStyle = 'hsla('+ color +', 100%, 75%, 0.3)';
+			c.strokeStyle = 'hsla('+ color +', 10%, 25%, 0.3)';
+
 			c.closePath();
 			c.fill();
 			c.stroke();
+
+			c.shadowColor = "transparent";
+			c.shadowOffsetX = c.shadowOffsetY = c.shadowBlur = 0;
+
+
+
+			c.beginPath();
+			for(var j = 0, jj = arcDivisions; j<=jj; j++){ // draw the inner arc of the filling slice, the one that represents completion
+				c.lineTo(
+					center.x + offset.x + Math.cos(subslice * j * sliceData.portion + slice * portionsToDate) * innerRadius,
+					center.y + offset.y + Math.sin(subslice * j * sliceData.portion + slice * portionsToDate) * innerRadius
+				)
+			}
+			for(var j = 0, jj = arcDivisions; j<=jj; j++){ // outer arc, with some math gymnastics to make it stay inside its borders
+				c.lineTo(
+					center.x + offset.x + Math.cos(subslice * (arcDivisions-j) * sliceData.portion + slice * portionsToDate) * ((outerRadius - innerRadius) * sliceData.percentage + innerRadius),
+					center.y + offset.y + Math.sin(subslice * (arcDivisions-j) * sliceData.portion + slice * portionsToDate) * ((outerRadius - innerRadius) * sliceData.percentage + innerRadius)
+				)
+			}
+			c.fillStyle = 'hsla('+ color +', 80%, 50%, 0.5)';
+			c.shadowColor = 'hsla('+color+', 100%, 50%, 0.99)';
+			c.strokeStyle = 'hsla('+ color +', 50%, 50%, 0.5)';
+
+			c.shadowOffsetX = 0;
+			c.shadowOffsetY = 0;
+			c.shadowBlur = outerRadius * 0.05;
+
+			c.closePath();
+			c.fill();
+			c.stroke();
+
+			c.shadowOffsetX = c.shadowOffsetY = c.shadowBlur = 0;
+
+			c.beginPath();
+			var d = 1 / pivot * outerRadius / 1.1;
+			var d2 = d + 1;
+			c.arc(center.x + offset.x * d,
+				  center.y + offset.y * d,
+				  3, -Math.PI, Math.PI);
+
+  			c.moveTo(center.x + offset.x * d2,
+	  				 center.y + offset.y * d2)
+			c.lineTo(center.x + offset.x * d * 1.3,
+				     center.y + offset.y * d * 1.3)
+			c.closePath();
+			c.strokeStyle = c.fillStyle = "#248";
+			c.stroke();
+			c.font = '20pt sans-serif';
+			c.shadowColor = '#666';
+			c.shadowBlur = 3;
+			c.shadowOffsetX = 1;
+			c.shadowOffsetY = 2;
+			c.textAlign = (offset.x > 0)?'left':'right';
+			c.fillText(sliceData.title, center.x + offset.x * d * 1.3 + offset.x / pivot * 12, center.y + offset.y * d * 1.3 + offset.y / pivot * 12);
+
+			c.shadowBlur =
+			c.shadowOffsetX =
+			c.shadowOffsetY = 0;
+
+			portionsToDate += sliceData.portion;
 		}//*/
+		// c.moveTo(center.x + outerRadius, center.y);
+		// c.arc(center.x, center.y, outerRadius, 0, Math.PI*2);
+		// c.stroke();
 	};
 	PiegressChart.prototype.update = function (title, percentage, color) {
 
@@ -94,9 +202,12 @@ $(function($){
 
 	var a = new Animator(document.getElementById("piegress-chart"));
 	var p = new PiegressChart();
-	p.addSlice('lol', 0.33, '#ff0');
-	p.addSlice('lol', 0.34, '#0ff');
-	p.addSlice('lol', 0.33, '#f0f');//*/
+	p.addSlice('Cynergie', 0.16, 25);
+	p.addSlice('Metaclub', 0.50, 20);
+	p.addSlice('LDAP', 0.89, 5);
+	p.addSlice('Projet "wow"', 0.33, 25);
+	p.addSlice('Blah', 0.14, 12);
+	p.addSlice('Plop', 0.75, 35);//*/
 
 	a.drawables.push(p);
 	a.start();
