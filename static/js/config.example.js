290100eefe49a9f6430a94782a0afdb26aa2279a
Dmitri Kolytchev <kolytchev.d@gmail.com>
2016-05-22 11:51:29 -0400

    adding sample config file

---
+++ static/js/config.example.js
@@ -0,0 +1,10 @@
+/**
+  * Sample config file, replace with own values
+  * the config.json file should contain the same key-value pairs if no in Dev mode.
+  */
+
+window.config = {
+	"org_url": "orgs/github",
+	"user": "example",
+	"token": "[TOKEN]"
+}
